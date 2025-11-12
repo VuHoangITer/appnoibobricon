64ffc8512521ad18a9e147b9e39c17c686217e45
VuHoangITer <vuvanhoang.1607@gmail.com>
2025-11-12 16:44:08 +0700

    Add PWA support (online-only)

---
+++ app/static/sw.js
@@ -0,0 +1,25 @@
+self.addEventListener('install', event => {
+  console.log('Service Worker installed');
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  console.log('Service Worker activated');
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .catch(err => {
+        console.log('Network request failed:', err);
+        return new Response('Không có kết nối internet', {
+          status: 503,
+          statusText: 'Service Unavailable',
+          headers: new Headers({
+            'Content-Type': 'text/plain'
+          })
+        });
+      })
+  );
+});
